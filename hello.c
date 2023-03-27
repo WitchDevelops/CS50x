39e33a9b416480e65c0d120489d18476a10f6204
Dominika Wojewska <112077394+WitchDevelops@users.noreply.github.com>
2023-03-27 23:08:55 +0200

    hello C
    I'm not scared, you're scared

---
+++ hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) //scream into the void
+{//is this a convention that is different from JavaScript?
+    printf("Hello, World! I am C \n"); //why is it print F, doesn't make any sense
+}

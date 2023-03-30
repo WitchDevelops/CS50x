48d9e98591bf9b2f2fdde2d714fe7983e1503e64
Dominika Wojewska <112077394+WitchDevelops@users.noreply.github.com>
2023-03-30 08:58:29 +0200

    Delete hello_user.c

--- hello_user.c
+++
@@ -1,9 +0,0 @@
-//include header files. they are libraries with functions. No inbuild functions in C
-#include <stdio.h>
-#include <cs50.h>
-
-int main(void)
-{
-    string answer = get_string("What is your name? ");
-    printf("Hello, %s!\n", answer);
-}

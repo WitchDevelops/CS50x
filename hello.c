4480f1f08081d062fc55f288527c2fd3287628d0
Dominika Wojewska <112077394+WitchDevelops@users.noreply.github.com>
2023-03-30 08:59:04 +0200

    Delete hello.c

--- hello.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(void) //scream into the void
-{//is this a convention that is different from JavaScript?
-    printf("Hello, World! I am C \n"); //why is it print F, doesn't make any sense
-}

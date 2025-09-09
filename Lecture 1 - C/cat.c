778bf232754e6cb6165e6f7bdbcf354b625be136
Dominika Wojewska <d.n.wojewska@gmail.com>
2025-09-09 14:54:47 +0200

    refactor: cat

--- Lecture 1 - C/cat.c
+++ Lecture 1 - C/cat.c
@@ -2,16 +2,28 @@
 #include <stdio.h>
 
 //this is meow function prototype
+int get_n(void);
 void meow(int number);
 
 int main(void)
+{   
+    int n = get_n();
+    meow(n);
+}
+
+int get_n(void)
 {
-    meow(3);
+    int n;
+    do
+    {
+        n = get_int("How many times the cat should meow? \n");
+    }
+    while (n <= 0);
 }
 
 void meow(int number)
 {   
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < number; i++)
     {
         printf("meow\n");
     }
